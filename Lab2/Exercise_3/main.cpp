704c05cdb09b3a89ab92c43a67ad683f6c7bf6fe
Giovanni Zaccaria <giozaccaria02@gmail.com>
2026-01-26 18:53:26 +0100

    Do exercise 1, 2, 3

--- Lab2/Exercise_3/main.cpp
+++ Lab2/Exercise_3/main.cpp
@@ -1,10 +1,10 @@
-#include "systemc.h"
 #include "EX_sc_signal.h"
+#include "systemc.h"
 
-int sc_main(int argc, char * argv[]) {
-	EX_sc_signal	inst("instance");
+int sc_main(int argc, char *argv[]) {
+    EX_sc_signal inst("instance");
 
-	sc_start();
+    sc_start();
 
-	return 0;
+    return 0;
 }
